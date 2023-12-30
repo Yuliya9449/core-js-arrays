1e65e5418bf71cc2666d8b9a88f541f32e499f2d
Yuliya Kotova <yulya9449@yandex.ru>
2023-12-30 03:42:12 +0300

    feat: add solution isValueEqualsIndex task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -163,10 +163,11 @@ function isSameLength(arr) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((elem, idx) => elem === idx);
 }
 
+//! 10
 /**
  * Inserts the item into specified array at specified index.
  *
