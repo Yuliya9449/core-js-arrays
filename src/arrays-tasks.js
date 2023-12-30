01dae00392046638620179cf6ac9acccbfad803e
Yuliya Kotova <yulya9449@yandex.ru>
2023-12-30 15:40:09 +0300

    feat: add solution createChunks task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -358,10 +358,13 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return Array.from({ length: Math.ceil(arr.length / chunkSize) }, () =>
+    arr.splice(0, chunkSize)
+  );
 }
 
+//! 21
 /**
  * Generates an array of odd numbers of the specified length.
  *
